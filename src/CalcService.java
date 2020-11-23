e34aa190bca1982b9b89c025511958a3255cea24
Denis Sitek <ugfan13@gmail.com>
2020-11-23 22:22:21 +0900

    New calc for annuitant

--- src/CalcService.java
+++ src/CalcService.java
@@ -1,8 +1,10 @@
 public class CalcService {
     public long calculate (long ammount, long year) {
-        long percent = 999;
-        long payment = ammount * percent / year / 12 / 100;
-        return payment;
+        double percent = 9.99 / 12 / 100;
+        double coefficient = Math.pow ((1 + percent), year * 12); // коэффициент возведения в степень
+        double annuitant = percent * coefficient / (coefficient - 1);
+        double payment = ammount * annuitant;
+        return (long) payment;
     }
 
 }
